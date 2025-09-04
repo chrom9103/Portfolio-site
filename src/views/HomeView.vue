62a6d9133e53385b3ae864289c8dc7b9682848c1
chrom9103 <kdkd21347.tm@gmail.com>
2025-09-04 16:35:02 +0900

    初期templeteをリセット

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,12 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <!-- HTML here -->
 </template>
+<style scoped>
+  /* CSS here */
+</style>
+<script setup>
+  // JavaScript here
+</script>
