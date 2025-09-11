677daa61fa7d38eb75bb16ecac2ed54c06dd0a4b
chrom9103 <kdkd21347.tm@gmail.com>
2025-09-11 18:18:49 +0900

    fix index.ts 追加ページのディレクトリを指定

--- src/router/index.ts
+++ src/router/index.ts
@@ -12,10 +12,17 @@ const router = createRouter({
     {
       path: '/about',
       name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue'),
+    },
+    {
+      path: '/works',
+      name: 'works',
+      component: () => import('../views/works/WorksView.vue'),
+    },
+    {
+      path: '/works/playground',
+      name: 'playground',
+      component: () => import('../views/works/PlayGround/MainView.vue'),
     },
   ],
 })
